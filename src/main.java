a63b1ebf852f952e30ba2de8ffb365c41ff6efcf
Ilyavosky <ilyacortesruiz@gmail.com>
2025-10-22 13:19:15 -0600

    Finalización de las actividades

--- src/main.java
+++ src/main.java
@@ -1,5 +1,64 @@
+import models.Navegation;
+import utils.NavegationUtils;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        NavegationUtils navegador = new NavegationUtils();
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
 
+        System.out.println("--- Simulador de Navegación Web ---");
+
+        while (!salir) {
+            System.out.println("\nElija una opción:");
+            System.out.println("1. Visitar nueva página");
+            System.out.println("2. Retroceder (Atrás)");
+            System.out.println("3. Avanzar (Adelante)");
+            System.out.println("4. Ver estado actual");
+            System.out.println("5. Salir");
+            System.out.print("Opción: ");
+
+            int opcion = 0;
+
+            try {
+                opcion = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Error: Por favor, ingrese un número válido.");
+                scanner.next();
+                continue;
+            }
+
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese la URL de la nueva página: ");
+                    String url = scanner.nextLine();
+                    navegador.goToNewPage(new Navegation(url));
+                    navegador.mostrarEstadoActual();
+                    break;
+                case 2:
+                    navegador.goBackwards();
+                    navegador.mostrarEstadoActual();
+                    break;
+                case 3:
+                    navegador.goForwards();
+                    navegador.mostrarEstadoActual();
+                    break;
+                case 4:
+                    navegador.mostrarEstadoActual();
+                    break;
+                case 5:
+                    salir = true;
+                    System.out.println("Cerrando el navegador. ¡Adiós!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+        }
+
+        scanner.close();
     }
 }
--- src/utils/TextEditorUtils.java
+++ src/utils/TextEditorUtils.java
@@ -11,18 +11,53 @@ public class TextEditorUtils {
     public TextEditorUtils() {
         this.RedoStack = new Stack<>();
         this.undoStack = new Stack<>();
-        this.currentEditor = null;
+        this.currentEditor = new TextEditor("");
     }
 
-    public void revertAction(TextEditor texReverted){
-        System.out.println("Revirtiendo la acción...");
+    public void revertAction(TextEditor newAction){
+        System.out.println("Nueva acción: '" + newAction.getText() + "'");
+
         if (this.currentEditor != null){
-              this.undoStack.push(this.currentEditor);
+            this.undoStack.push(this.currentEditor);
         }
 
-        this.currentEditor =  texReverted;
+        this.currentEditor =  newAction;
+
         this.RedoStack.clear();
     }
 
 
+    public void undoAction() {
+        if (this.undoStack.isEmpty()) {
+            System.out.println("No hay acciones que deshacer.");
+            return;
+        }
+
+        this.RedoStack.push(this.currentEditor);
+
+        this.currentEditor = this.undoStack.pop();
+        System.out.println("DESHACER. El texto ahora es: '" + this.currentEditor.getText() + "'");
+    }
+
+
+    public void redoAction() {
+        if (this.RedoStack.isEmpty()) {
+            System.out.println("No hay acciones que rehacer.");
+            return;
+        }
+
+        this.undoStack.push(this.currentEditor);
+
+        this.currentEditor = this.RedoStack.pop();
+        System.out.println("REHACER. El texto ahora es: '" + this.currentEditor.getText() + "'");
+    }
+
+
+    public void ShowStatus() {
+        System.out.println("---------------------------------");
+        System.out.println("Pila Deshacer (undoStack): " + this.undoStack.size() + " estados");
+        System.out.println("Texto Actual: '" + (this.currentEditor != null ? this.currentEditor.getText() : "") + "'");
+        System.out.println("Pila Rehacer (RedoStack): " + this.RedoStack.size() + " estados");
+        System.out.println("---------------------------------");
+    }
 }
